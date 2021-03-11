6b3a5511251fcc730aebaf4dc523eab37a3cd4b9
Johannes Kettmann <jkettmann@gmx.net>
2021-03-11 06:47:19 +0100

    Add theme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import { Normalize } from 'styled-normalize';
+import GlobalStyle from './GlobalStyle';
+import theme from './theme';
 
 function App() {
   return (
-    <Router>
-      <Normalize />
-      <Switch>
-        <Route path="/search">Search</Route>
-        <Route path="/">Home</Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Normalize />
+        <GlobalStyle />
+        <Switch>
+          <Route path="/search">Search</Route>
+          <Route path="/">Home</Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
